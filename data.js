e3f117d73963c3441d8bea7c6067c35f03e2da41
NickMeidinger <n.meidinger@we-brand-you.de>
2025-07-22 15:42:53 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,359 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-foto-studio",
+      "name": "Foto Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.2637928349046703,
+        "pitch": 0.3250883065772019,
+        "fov": 1.0056298413644362
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08310540046144332,
+          "pitch": 0.13115207370160675,
+          "rotation": 0,
+          "target": "3-besprechungsraum"
+        },
+        {
+          "yaw": -1.7014244791037,
+          "pitch": 0.24727603588680935,
+          "rotation": 0,
+          "target": "1-podcast-studio"
+        },
+        {
+          "yaw": -1.148197729501769,
+          "pitch": 0.22285162765792066,
+          "rotation": 0,
+          "target": "2-schreibtische"
+        },
+        {
+          "yaw": -0.3035464948994431,
+          "pitch": 0.05864874469016179,
+          "rotation": 0,
+          "target": "4-empfangsbereich"
+        },
+        {
+          "yaw": -0.8343082551766265,
+          "pitch": 0.0728118239537956,
+          "rotation": 0,
+          "target": "5-lager"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-podcast-studio",
+      "name": "Podcast Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7183922953900819,
+          "pitch": 0.2093604613335991,
+          "rotation": 0,
+          "target": "0-foto-studio"
+        },
+        {
+          "yaw": 1.118131416676027,
+          "pitch": 0.02719676919963554,
+          "rotation": 0,
+          "target": "3-besprechungsraum"
+        },
+        {
+          "yaw": 0.27039202737345036,
+          "pitch": 0.22065278327905524,
+          "rotation": 0,
+          "target": "2-schreibtische"
+        },
+        {
+          "yaw": -0.00478277818175421,
+          "pitch": 0.013511545323328988,
+          "rotation": 0,
+          "target": "5-lager"
+        },
+        {
+          "yaw": 0.6240592950695039,
+          "pitch": -0.006545849402133541,
+          "rotation": 0,
+          "target": "4-empfangsbereich"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-schreibtische",
+      "name": "Schreibtische",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16619177956999565,
+          "pitch": 0.07107412522044676,
+          "rotation": 0,
+          "target": "5-lager"
+        },
+        {
+          "yaw": 0.8639752096624083,
+          "pitch": -0.0010698295784123957,
+          "rotation": 0,
+          "target": "4-empfangsbereich"
+        },
+        {
+          "yaw": 1.5286074219339536,
+          "pitch": 0.0693245981746422,
+          "rotation": 0,
+          "target": "3-besprechungsraum"
+        },
+        {
+          "yaw": 2.4945139137060606,
+          "pitch": 0.28627662958095,
+          "rotation": 0,
+          "target": "0-foto-studio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-besprechungsraum",
+      "name": "Besprechungsraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.03747538982162,
+          "pitch": 0.19996389494112776,
+          "rotation": 0,
+          "target": "0-foto-studio"
+        },
+        {
+          "yaw": 2.5497470604071495,
+          "pitch": 0.09560989665948583,
+          "rotation": 0,
+          "target": "1-podcast-studio"
+        },
+        {
+          "yaw": 2.865311294765098,
+          "pitch": 0.14696887221161248,
+          "rotation": 0,
+          "target": "2-schreibtische"
+        },
+        {
+          "yaw": -2.698866634304018,
+          "pitch": 0.08426421645390114,
+          "rotation": 1.5707963267948966,
+          "target": "5-lager"
+        },
+        {
+          "yaw": -2.4517669598622494,
+          "pitch": 0.2657882088484733,
+          "rotation": 1.5707963267948966,
+          "target": "4-empfangsbereich"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-empfangsbereich",
+      "name": "Empfangsbereich",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2416492666299082,
+          "pitch": 0.19425271646485598,
+          "rotation": 4.71238898038469,
+          "target": "0-foto-studio"
+        },
+        {
+          "yaw": -0.45609748478062073,
+          "pitch": 0.05407328135927969,
+          "rotation": 1.5707963267948966,
+          "target": "5-lager"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-lager",
+      "name": "Lager",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9815923571901024,
+          "pitch": 0.36104643696780414,
+          "rotation": 4.71238898038469,
+          "target": "0-foto-studio"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Büro WBY",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
